5f81a7fcc451c6fb2747c4e7ffd17d27264307ac
a-ww2-er <warhistories101@gmail.com>
2024-04-12 04:37:44 +0300

    added fix

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -147,7 +147,7 @@ export const getCourseByUser = CatchAsyncError(
 
 
       const courseExists = userCourseList?.find(
-        (course: any) => course._id.toString() === courseId
+        (course: any) => course.courseId.toString() === courseId
       );
 
       if (!courseExists) {
@@ -333,7 +333,7 @@ export const addReview = CatchAsyncError(
 
       // check if courseId already exists in userCourseList based on _id
       const courseExists = userCourseList?.some(
-        (course: any) => course._id.toString() === courseId.toString()
+        (course: any) => course.courseId.toString() === courseId.toString()
       );
 
       if (!courseExists) {
